7281e9082d0ae09f049c9581ca369cc9f8591cc5
Fitz Hawke <will@fitzhawk.com>
2022-09-24 14:07:55 -0400

    bring into line with source - add passport.js auth

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
+const passport = require("passport");
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
+
+require("./config/passport");
 
 connectDB();
 
@@ -14,7 +18,21 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(errorHandler);
 
+app.use(passport.initialize());
+
 app.use('/api/workouts', require('./routes/workoutRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
 
 app.listen(port, () => console.log(`server started on port ${port}`));
