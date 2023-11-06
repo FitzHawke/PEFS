2a03abbe3364ed869c370116215a5863161f8571
FitzHawke <will@fitzhawk.com>
2023-11-05 20:39:26 -0500

    fix: extra section showing in main dash

--- frontend/src/pages/MainDash.jsx
+++ frontend/src/pages/MainDash.jsx
@@ -13,6 +13,7 @@ import {
 } from "../features/weight/weightSlice";
 
 function MainDash() {
+  const featWeight = false;
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -82,7 +83,7 @@ function MainDash() {
       <div className="flex flex-col lg:flex-row gap-4 justify-around w-full h-full">
         <DashSection data={runs} type="Run" title="Runs" />
         <DashSection data={rides} type="Ride" title="Bike Rides" />
-        <DashSection data={weights} type="Weight" title="Weights" />
+        {featWeight && <DashSection data={weights} type="Weight" title="Weights" />}
       </div>
     </div>
   );
